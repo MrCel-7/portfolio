7a1148c8d06617e692734d378e03ac7d39765525
Marcel Wang <mrcel.wng@gmail.com>
2026-01-14 21:25:24 +0700

    Update Skill Pop Up

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,15 @@ const App = () => {
   const [isAnimating, setIsAnimating] = useState(false);
 
   const [showAbout, setShowAbout] = useState(false);
+  const [showSkill, setShowSkill] = useState(false);
+  const [showProject, setShowProject] = useState(false);
+  const [showContact, setShowContact] = useState(false);
+
+  const javascriptPercent = 76;
+  const reactPercent = 73;
+  const tailwindPercent = 92;
+  const nodeJsPercent = 47;
+  const mySql = 67;
 
   const THRESHOLD = 160;
 
@@ -223,6 +232,90 @@ const App = () => {
             </div>
           )}
 
+          {/* Show Skill */}
+          {showSkill && (
+            <div className="fixed inset-0 z-50 px-10 grid place-items-center">
+              {/* overlay */}
+              <div
+                className="absolute inset-0"
+                onClick={() => setShowSkill(false)}
+              />
+
+              {/* popup */}
+              <div className="relative w-full mx-10">
+                <div className="about-pop rounded-xl bg-white/30 border border-white/10 backdrop-blur-lg p-6 text-white">
+                  <div className="flex justify-between items-center mb-4">
+                    <h1 className="text-2xl font-bold text-white">Skills</h1>
+                    <button onClick={() => setShowSkill(false)}>
+                      <X size={20} />
+                    </button>
+                  </div>
+
+                  <div className="flex flex-col">
+                    <div className="flex w-full justify-between">
+                      <h1 className="text-xl">Javascript</h1>
+                      <span>{javascriptPercent}%</span>
+                    </div>
+                    <div className="w-full rounded-full overflow-hidden bg-white/30 backdrop-blur-lg">
+                      <div
+                        className="bg-yellow-400 py-1"
+                        style={{ width: `${javascriptPercent}%` }}
+                      ></div>
+                    </div>
+                  </div>
+                  <div className="flex mt-5 flex-col">
+                    <div className="flex w-full justify-between">
+                      <h1 className="text-xl">React</h1>
+                      <span>{reactPercent}%</span>
+                    </div>
+                    <div className="w-full rounded-full overflow-hidden bg-white/30 backdrop-blur-lg">
+                      <div
+                        className="bg-blue-400 py-1"
+                        style={{ width: `${reactPercent}%` }}
+                      ></div>
+                    </div>
+                  </div>
+                  <div className="flex mt-5 flex-col">
+                    <div className="flex w-full justify-between">
+                      <h1 className="text-xl">TailwindCSS</h1>
+                      <span>{tailwindPercent}%</span>
+                    </div>
+                    <div className="w-full rounded-full overflow-hidden bg-white/30 backdrop-blur-lg">
+                      <div
+                        className="bg-blue-200 py-1"
+                        style={{ width: `${tailwindPercent}%` }}
+                      ></div>
+                    </div>
+                  </div>
+                  <div className="flex mt-5 flex-col">
+                    <div className="flex w-full justify-between">
+                      <h1 className="text-xl">NodeJS</h1>
+                      <span>{nodeJsPercent}%</span>
+                    </div>
+                    <div className="w-full rounded-full overflow-hidden bg-white/30 backdrop-blur-lg">
+                      <div
+                        className="bg-green-400 py-1"
+                        style={{ width: `${nodeJsPercent}%` }}
+                      ></div>
+                    </div>
+                  </div>
+                  <div className="flex mt-5 flex-col">
+                    <div className="flex w-full justify-between">
+                      <h1 className="text-xl">MySQL</h1>
+                      <span>{mySql}%</span>
+                    </div>
+                    <div className="w-full rounded-full overflow-hidden bg-white/30 backdrop-blur-lg">
+                      <div
+                        className="bg-orange-400 py-1"
+                        style={{ width: `${mySql}%` }}
+                      ></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+
           {/* Bottom Apps Icon */}
           <div className="absolute border-t-3 border-white/10 bg-white/10 backdrop-blur-sm flex gap-5 bottom-0 w-full justify-center py-5">
             <div
@@ -231,7 +324,10 @@ const App = () => {
             >
               <Scroll size={28} className="text-white font-bold" />
             </div>
-            <div className="w-15 hover:scale-110 cursor-pointer h-15 rounded-full flex border-3 justify-center items-center border-white">
+            <div
+              onClick={() => setShowSkill(true)}
+              className="w-15 hover:scale-110 cursor-pointer h-15 rounded-full flex border-3 justify-center items-center border-white"
+            >
               <BsLightning size={28} className="text-white font-bold" />
             </div>
             <div className="w-15 hover:scale-125 scale-110 cursor-pointer h-15 rounded-full flex border-3 justify-center items-center border-white">
