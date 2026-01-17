e02be6eba209d5e78bf989fc968f575693c58b18
Marcel Wang <mrcel.wng@gmail.com>
2026-01-17 14:02:32 +0700

    Efek langsung hapus saat ganti card

--- src/App.jsx
+++ src/App.jsx
@@ -145,7 +145,7 @@ const App = () => {
 
       {/* HOME SCREEN */}
       <div className="h-screen flex items-center justify-center">
-        <img src="/girl.png" className="w-full h-screen object-cover" />
+        <img src="/tag.jpeg" className="w-full h-screen object-cover" />
         <div className="flex bg-black/10 w-full h-screen absolute top-0 left-0">
           {/* Top Notification */}
           <div className="absolute py-1 border-b border-white/10 items-center flex justify-between px-5 top-0 text-white bg-white/10 backdrop-blur-lg w-full">
@@ -317,15 +317,21 @@ const App = () => {
           )}
 
           {/* Bottom Apps Icon */}
-          <div className="absolute border-t-3 border-white/10 bg-white/10 backdrop-blur-sm flex gap-5 bottom-0 w-full justify-center py-5">
+          <div className="absolute border-t-3 z-100 border-white/10 bg-white/10 backdrop-blur-sm flex gap-5 bottom-0 w-full justify-center py-5">
             <div
-              onClick={() => setShowAbout(true)}
+              onClick={() => {
+                setShowAbout(true);
+                setShowSkill(false);
+              }}
               className="w-15 hover:scale-110 cursor-pointer h-15 rounded-full flex border-3 justify-center items-center border-white"
             >
               <Scroll size={28} className="text-white font-bold" />
             </div>
             <div
-              onClick={() => setShowSkill(true)}
+              onClick={() => {
+                setShowSkill(true);
+                setShowAbout(false);
+              }}
               className="w-15 hover:scale-110 cursor-pointer h-15 rounded-full flex border-3 justify-center items-center border-white"
             >
               <BsLightning size={28} className="text-white font-bold" />
