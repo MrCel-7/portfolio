6c2da3219ae913655312a4ff161ff3899805fac5
Marcel Wang <mrcel.wng@gmail.com>
2026-01-21 16:32:55 +0700

    Update Google

--- src/pages/ShowBrowser.jsx
+++ src/pages/ShowBrowser.jsx
@@ -20,55 +20,71 @@ const ShowBrowser = ({ setShowChrome, age }) => {
       {/* Body */}
       <div className="w-full gap-2 h-screen p-2 flex flex-col">
         <h1 className="text-3xl mt-9 font-semibold">Marcel Wang</h1>
-        <p className="text-md">Web Developer</p>
+        <p className="text-sm">Web Developer</p>
         <div className="py-1 px-3 rounded-full border w-fit">
-          <p className="text-md font-semibold">Informasi Umum</p>
+          <p className="text-sm font-semibold">General Information</p>
         </div>
 
         {/* Images */}
         <div className="rounded-xl overflow-hidden">
           <div className="flex gap-2 overflow-x-auto scroll-smooth snap-x snap-mandatory no-scrollbar">
             {/* Image 1 */}
-            <div className="min-w-60 bg-green-300 snap-center overflow-hidden">
+            <div className="min-w-50 bg-green-300 snap-center overflow-hidden">
               <img
                 src="/gambar-1.png"
-                className="w-60 h-60 object-cover"
+                className="w-50 h-50 object-cover"
                 draggable={false}
               />
             </div>
 
             {/* Image 2 */}
-            <div className="bg-yellow-300 min-w-60 snap-center overflow-hidden">
+            <div className="bg-yellow-300 min-w-50 snap-center overflow-hidden">
               <img
                 src="/gambar-2.png"
-                className="w-60 h-60 object-cover"
+                className="w-50 h-50 object-cover"
                 draggable={false}
               />
             </div>
 
             {/* Image 3 */}
-            <div className="bg-yellow-300 min-w-60 snap-center overflow-hidden">
+            <div className="bg-yellow-300 min-w-50 snap-center overflow-hidden">
               <img
                 src="/girl.png"
-                className="w-60 h-60 object-cover"
+                className="w-50 h-50 object-cover"
                 draggable={false}
               />
             </div>
           </div>
         </div>
 
-        {/* Age */}
+        {/* Information */}
         <div className="flex w-full gap-2">
           <div className="rounded-xl p-2 bg-gray-200 flex-1">
-            <p className="text-md">Age</p>
-            <p className="text-2xl font-semibold mt-3">{age} years old</p>
-            <p className="text-md">March 22, 2004</p>
+            <p className="text-sm">Age</p>
+            <p className="text-xl font-semibold mt-3">{age} years old</p>
+            <p className="text-sm">March 22, 2004</p>
           </div>
           <div className="rounded-xl p-2 bg-gray-200 flex-1">
-            <p className="text-md">Religion</p>
-            <p className="text-2xl font-semibold mt-3">Christian</p>
-            <p className="text-md">March 22, 2004</p>
+            <p className="text-sm">Religion</p>
+            <p className="text-xl font-semibold mt-3">Christian</p>
           </div>
+        </div>
+
+        {/* Summary */}
+        <div className="w-full rounded-xl p-2 bg-gray-200">
+          <p className="text-sm font-bold">Summary</p>
+          <p className="text-sm mt-3">
+            Marcel Wang is an Indonesian Web Developer with a strong passion for
+            coding and web technologies. He grew up in Tangerang City and
+            graduated from SMK Mulia Buana in 2021. Coding started as a hobby
+            for Marcel and gradually became his professional focus. He is
+            skilled in modern web technologies such as JavaScript, ReactJS, and
+            Tailwind CSS, which he uses to build interactive, responsive, and
+            visually appealing web interfaces. Driven by curiosity and
+            continuous learning, Marcel is committed to creating efficient,
+            user-friendly digital solutions and constantly improving his craft
+            as a web developer.
+          </p>
         </div>
       </div>
 
